6c5198b27697c7f9662b12a3ad1870644921c12f
Pankaj Sherchan <pshercha@berkadia.com>
2020-05-18 21:22:52 -0400

    user controller and services refactor

--- server/src/application/users/users.controller.js
+++ server/src/application/users/users.controller.js
@@ -1,18 +1,22 @@
-import httpStatusCode from 'http-status-codes';
-import env from '../../env';
-import { hashPassword, verifyToken } from '../../services/authentication.service';
+import httpStatusCodes from 'http-status-codes';
 import * as userService from '../../services/user.service';
-import { HTTP_RESPONSE_MESSAGES } from '../../shared/messages';
 import logger from '../../utils';
 import catchAsync from '../../utils/catchAsync';
+import checkResponse from '../../utils/responseHandler';
 import AppError from '../error/appError';
 
-export const createUser = catchAsync(async (req, res) => {
-  logger.info('UserController - CreateUser');
+export const addUser = catchAsync(async (req, res, next) => {
+  logger.info('UserController - addUser');
   const reqUser = req.body;
   const { username, email } = req.body;
 
-  const result = await userService.saveUser(reqUser);
+  const userExists = await userService.getUser(username);
+
+  if (checkResponse(userExists)) {
+    return next(new AppError('User already exits', 404));
+  }
+
+  const result = await userService.addUser(reqUser);
   await userService.sendUserVerificationEmail(username, email);
   res.send(result);
 });
@@ -22,119 +26,24 @@ export const updateUser = catchAsync(async (req, res) => {
   res.send(result);
 });
 
-export const login = catchAsync(async (req, res) => {
-  logger.info('UserController - login');
-  const token = await userService.loginUser(req.body);
-  res.json(token);
-});
-
-/**
- *returns the user by username
- *@reqParam username
- * @param {*} req
- * @param {*} res
- */
 export const getUser = catchAsync(async (req, res, next) => {
   logger.info('UserController - GetUser');
 
   const { username } = req.params;
-  const result = await userService.getUser({ username });
+  const userResponse = await userService.getUser({ username });
 
-  if (!result || !result.data) {
-    return next(new AppError(`No user with username ${username} found`, 404));
+  if (checkResponse(userResponse)) {
+    return next(
+      new AppError(`No user with username ${username} found`, httpStatusCodes.BAD_REQUEST)
+    );
   }
 
-  res.send(result);
+  res.send(userResponse);
 });
 
-/**
- * verifies user and updates the user's verified field to true
- * @reqParam tokenId
- * @param {*} req
- * @param {*} res
- */
-export const verifyUser = async (req, res) => {
-  logger.info('USER Controller - Verify User');
+export const deleteUser = catchAsync(async (req, res, next) => {
+  logger.info('UserController - DeleteUser');
 
-  if (!req.params.tokenId) {
-    res.send(HTTP_RESPONSE_MESSAGES.VERIFICATION.USER_VERIFICATION_DATA_NOT_FOUND);
-  }
-
-  let result = {};
-
-  const { tokenId } = req.params;
-
-  try {
-    const data = await verifyToken(tokenId, env.VERIFY_USER_SECRET_KEY);
-    const { tokenData: username } = data;
-
-    const userInDatabase = await userService.getUser({ username });
-
-    const user = userInDatabase.data;
-    user.verified = true;
-
-    await userService.updateUser(user);
-
-    result = {
-      username,
-      httpStatus: httpStatusCode.OK,
-      message: HTTP_RESPONSE_MESSAGES.VERIFICATION.USER_VERIFIED_SUCCESSFUL
-    };
-  } catch (error) {
-    logger.error(`USER CONTROLLER - Verify User Error ${error}`);
-
-    result = {
-      data: null,
-      httpStatus: httpStatusCode.BAD_REQUEST,
-      message: HTTP_RESPONSE_MESSAGES.VERIFICATION.USER_VERIFIED_FAILED
-    };
-  }
-
-  res.send(result);
-};
-
-export const changePassword = async (req, res) => {
-  const { username, password } = req.body;
-
-  let result = {};
-
-  try {
-    const userInDatabase = await userService.getUser({ username });
-    console.log('userInDatabase: ', userInDatabase);
-
-    console.log(!userInDatabase.data);
-
-    if (!userInDatabase.data) {
-      result = {
-        data: null,
-        httpStatus: httpStatusCode.NOT_FOUND,
-        message: 'User not found'
-      };
-
-      return res.send(result);
-    }
-
-    const user = userInDatabase.data;
-
-    const hashedPassword = await hashPassword({ password });
-    user.password = hashedPassword;
-
-    await userService.updateUser(user);
-
-    result = {
-      user,
-      httpStatus: httpStatusCode.OK,
-      message: HTTP_RESPONSE_MESSAGES.VERIFICATION.USER_PASSWORD_CHANGE_SUCCESSFUL
-    };
-  } catch (error) {
-    logger.error(`USER CONTROLLER - Verify User Error ${error}`);
-
-    result = {
-      data: null,
-      httpStatus: httpStatusCode.BAD_REQUEST,
-      message: HTTP_RESPONSE_MESSAGES.VERIFICATION.USER_PASSWORD_CHANGE_FAILED
-    };
-  }
-
-  return res.send(result);
-};
+  const response = await userService.deleteUser();
+  res.send(response);
+});
--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,106 +1,101 @@
-import httpStatus from 'http-status-codes';
+import httpStatusCodes from 'http-status-codes';
 import AppError from '../application/error/appError';
 import User from '../database/schemas/user.schema';
 import env from '../env';
-import { HTTP_RESPONSE_MESSAGES } from '../shared/messages';
 import logger from '../utils';
-import checkResponse from '../utils/responseHandler';
 import { generateToken, hashPassword } from './authentication.service';
 import sendEmail from './email.service';
 
 const messages = {
   USER_ALREADY_EXIST: 'User already exist',
   USER_CREATED_FAILED: 'User creation failed. Please try again',
-  USER_CREATED_SUCCESS: 'User created successfully'
+  USER_CREATED_SUCCESS: 'User created successfully',
+  USER_UPDATE_FAILED: 'User update failed. Please try again',
+  USER_DELETE_FAILED: 'User delete failed. Please try again'
 };
 
-export const getUser = async ({ username }) => {
-  logger.info('User Service - GetUser', { meta: username });
+export const getUsers = async () => {
+  logger.info('User Service - GetUsers');
 
-  let result = {};
+  try {
+    return {
+      data: await User.find(),
+      httpStatus: httpStatusCodes.OK
+    };
+  } catch (error) {
+    throw new AppError('Could not get users', httpStatusCodes.BAD_REQUEST);
+  }
+};
+
+export const getUser = async username => {
+  logger.info('User Service - GetUser', { meta: username });
 
   try {
     const user = await User.findOne({ username });
-    result = {
+    return {
       data: user,
-      httpStatus: httpStatus.OK,
-      message: HTTP_RESPONSE_MESSAGES.USER.USER_GET_SUCCESSFUL
+      httpStatus: httpStatusCodes.OK
     };
   } catch (error) {
     logger.error('UserService - getUser', { meta: error });
 
-    result = {
-      data: null,
-      httpStatus: httpStatus.BAD_REQUEST,
-      message: HTTP_RESPONSE_MESSAGES.USER.USER_GET_FAILED
-    };
-
-    throw error;
+    throw new AppError(`Could not get user with username ${username}`, httpStatusCodes.BAD_REQUEST);
   }
-
-  return result;
 };
 
-export const saveUser = async user => {
-  logger.info('UserService - SaveUser');
+export const createUser = async user => {
+  logger.info('UserService - Create User');
 
   const { username, firstName, lastName, email, password } = user;
 
+  const newUser = new User({
+    username,
+    firstName,
+    lastName,
+    email
+  });
+
+  const hashedPassword = await hashPassword({ password });
+  newUser.password = hashedPassword;
+
   try {
-    const userInDatabase = await getUser({ username });
-    if (checkResponse(userInDatabase)) {
-      throw new AppError(messages.USER_ALREADY_EXIST, httpStatus.BAD_REQUEST);
-    }
-
-    const newUser = new User({
-      username,
-      firstName,
-      lastName,
-      email
-    });
-
-    const hashedPassword = await hashPassword({ password });
-    newUser.password = hashedPassword;
-
-    user = await newUser.save();
-
-    if (!user) {
-      throw new AppError(messages.USER_CREATED_FAILED, httpStatus.BAD_REQUEST);
-    }
-
-    logger.info('Userservice - save user - successful');
+    logger.info('User service - create user - successful');
 
     return {
-      data: user,
-      httpStatus: httpStatus.OK
+      data: await newUser.save(),
+      httpStatus: httpStatusCodes.OK
     };
   } catch (error) {
-    throw new AppError(error.message, httpStatus.BAD_REQUEST);
+    throw new AppError(messages.USER_CREATED_FAILED, httpStatusCodes.BAD_REQUEST);
   }
 };
 
 export const updateUser = async user => {
   logger.info('User Service - update user');
-
-  let result;
-
   try {
     const updatedUser = await User.updateOne({ _id: user._id }, user);
 
-    result = {
+    return {
       data: updatedUser,
-      httpStatus: httpStatus.OK,
-      message: HTTP_RESPONSE_MESSAGES.USER.USER_UPDATED_SUCCESSFUL
+      httpStatus: httpStatusCodes.OK
     };
   } catch (err) {
-    result = {
-      data: null,
-      httpStatus: httpStatus.BAD_REQUEST,
-      message: err
-    };
+    throw new AppError(messages.USER_UPDATE_FAILED, httpStatusCodes.BAD_REQUEST);
   }
+};
 
-  return result;
+export const deleteUser = async id => {
+  logger.info('User Service - delete user');
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+
+    return {
+      data: deletedUser,
+      httpStatus: httpStatusCodes.OK
+    };
+  } catch (err) {
+    throw new AppError(messages.USER_DELETE_FAILED, httpStatusCodes.BAD_REQUEST);
+  }
 };
 
 export const sendUserVerificationEmail = async (username, email) => {
